5152333b7cb916c8e5b8a2bf64e9a6c7e6ec69e6
Steffen Brand <s.brand@steffenbrand.com>
2022-12-09 15:18:58 +0100

    Implement idea for Day9

--- src/Year2022/Day9/Part1.php
+++ src/Year2022/Day9/Part1.php
@@ -51,3 +51,40 @@ class Part1
         $this->positionsVisitedByTail[$this->tailPosition->getUniqueIdentifier()] = 1;
     }
 }
+
+class Position
+{
+    private int $vertical;
+    private int $horizontal;
+
+    public function __construct(int $vertical, int $horizontal)
+    {
+        $this->horizontal = $horizontal;
+        $this->vertical = $vertical;
+    }
+
+    public function getUniqueIdentifier(): string
+    {
+        return sprintf('v%dh%d', $this->horizontal, $this->vertical);
+    }
+
+    public function getHorizontal(): int
+    {
+        return $this->horizontal;
+    }
+
+    public function setHorizontal(int $horizontal): void
+    {
+        $this->horizontal = $horizontal;
+    }
+
+    public function getVertical(): int
+    {
+        return $this->vertical;
+    }
+
+    public function setVertical(int $vertical): void
+    {
+        $this->vertical = $vertical;
+    }
+}
